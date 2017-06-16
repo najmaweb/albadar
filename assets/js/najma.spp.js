59627ca8a07440286fcf683918a070ecf0d73003
pujie <pw.prayitno@gmail.com>
2017-06-17 06:52:49 +0700

    rearrange najma.spp.js

--- assets/js/najma.spp.js
+++ assets/js/najma.spp.js
@@ -5,7 +5,6 @@ numberWithCommas = function(x) {
     return parts.join(".");
 }
 removeCommas = function(myVar){
-    //myVar = myVar.replace(/([.,])(\d\d\d\D|\d\d\d$)/g,'$2');
     myVar = myVar.replace(/[ ,]/g, "");
     return myVar;
 }
@@ -16,68 +15,6 @@ filltotal = function(){
 fillreturnmoney = function(){
     $("#returnmoney").val(numberWithCommas(parseInt(removeCommas($("#cashpay").val()))-parseInt(removeCommas($("#total").val()))));
 }
-$.ajax({
-    url:'/students/getjson',
-    dataType:'json'
-})
-.done(function(res){
-    $("#sname").focus(function(){
-        $(this).select();
-    });
-    $('#sname').autocomplete({
-        lookup: res.out,
-        onSelect: function(suggestion) {
-            console.log('suggestion-data',suggestion.data);
-            console.log('suggestion',suggestion);
-            console.log('cek status');
-            $("#nis").val(suggestion.nis);
-            $("#studentname").val(suggestion.name);
-            $("#grade").val(suggestion.grade);
-                $.ajax({
-                    url:'/students/getproperties/'+suggestion.nis,
-                    type:'get',
-                    dataType:'Json'
-                })
-                .done(function(res){
-                    originspp = res.spp;
-                    $("#spp_").val(numberWithCommas(res.spp));
-                    $("#spp").val(res.spp);
-                    $("#bimbel_").val(numberWithCommas(res.bimbel));
-                    $("#bimbel").val(res.bimbel);
-                    $("#oribimbel").val(res.bimbel);
-                    filltotal();
-                    fillreturnmoney();
-                });
-            //$('#selction-bts').html('You selected: ' + suggestion.value + ', ' + suggestion.data);
-        },
-        onHint: function (hint) {
-            console.log('hint',hint);
-            //$('#autocomplete-bts-x').val(hint);
-        },
-        onInvalidateSelection: function() {
-            //$('#selction-bts').html('You selected: none');
-        }
-    });
-})
-.fail(function(err){
-    console.log("Error fetch json",err);
-});
-$("#cashpay").keyup(function(){
-    fillreturnmoney()
-});
-$("#cashpay").focus(function(){
-    $(this).select();
-})
-$("#cashpay").blur(function(){
-    $(this).val(numberWithCommas($(this).val()));
-})
-$(".affect-total").focus(function(){
-    $(this).val(removeCommas($(this).val().toString()));
-    $(this).select();
-})
-$(".affect-total").blur(function(){
-    $(this).val(numberWithCommas($(this).val().toString()));
-})
 togglespp = function(){
     if($("#sppcheckbox").prop("checked")){
         $("#sppmonthdiv").show();
@@ -85,10 +22,6 @@ togglespp = function(){
         $("#sppmonthdiv").hide();
     }
 }
-togglespp();
-$("#sppcheckbox").change(function(){
-    togglespp();
-})
 togglebimbel = function(){
     if($("#bimbelcheckbox").prop("checked")){
         $("#bimbelmonthdiv").show();
@@ -96,15 +29,6 @@ togglebimbel = function(){
         $("#bimbelmonthdiv").hide();
     }
 }
-togglebimbel();
-$("#bimbelcheckbox").change(function(){
-    togglebimbel();
-});
-$(".affect-total").keyup(function(){
-    filltotal();
-//    $("#returnmoney").val(parseInt($("#cashpay").val())-total);
-    fillreturnmoney();
-});
 $.fn.adjustval = function(options){
     $(this).change(function(){
         var settings = $.extend({
@@ -143,43 +67,95 @@ $.fn.adjustval = function(options){
         if(test===true){
             alert(monthcount);
         };
-
     });
 };
-$(".bimberperiod").adjustval({
-        firstmonth:$("#frstmonth"),
-        firstyear:$("#frstyear"),
-        nextmonth:$("#nextmonth"),
-        nextyear:$("#nextyear"),
-        hiddenval : $("#bimbel"),
-        shownval: $("#bimbel_"),
-        orival: $("#oribimbel")
-});
-$(".sppperiod").change(function(){
-    console.log("time changed");
-    monthcount = 0;
-    if($("#sppnextyear").val()===$("#sppfrstyear").val()){
-        if($("#sppnextmonth").val()===$("#sppfrstmonth").val()){
-            monthcount = 1;
-        }else
-        if($("#sppnextmonth").val()>$("#sppfrstmonth").val()){
-            monthcount = $("#sppnextmonth").val() - $("#sppfrstmonth").val() + 1;
-        }else{
-            alert("Bulan Kedua harus lebih besar dari bulan Pertama");
+$.ajax({
+    url:'/students/getjson',
+    dataType:'json'
+})
+.done(function(res){
+    $("#sname").focus(function(){
+        $(this).select();
+    });
+    $('#sname').autocomplete({
+        lookup: res.out,
+        onSelect: function(suggestion) {
+            console.log('suggestion-data',suggestion.data);
+            console.log('suggestion',suggestion);
+            console.log('cek status');
+            $("#nis").val(suggestion.nis);
+            $("#studentname").val(suggestion.name);
+            $("#grade").val(suggestion.grade);
+                $.ajax({
+                    url:'/students/getproperties/'+suggestion.nis,
+                    type:'get',
+                    dataType:'Json'
+                })
+                .done(function(res){
+                    originspp = res.spp;
+                    $("#spp_").val(numberWithCommas(res.spp));
+                    $("#spp").val(res.spp);
+                    $("#orispp").val(res.spp);
+                    $("#bimbel_").val(numberWithCommas(res.bimbel));
+                    $("#bimbel").val(res.bimbel);
+                    $("#oribimbel").val(res.bimbel);
+                    filltotal();
+                    fillreturnmoney();
+                });
+        },
+        onHint: function (hint) {
+            console.log('hint',hint);
+        },
+        onInvalidateSelection: function() {
         }
-    }else
-    if($("#sppnextyear").val()>$("#sppfrstyear").val()){
-        frsmonths = 12 - $("#sppfrstmonth").val();
-        months = 12*($("#sppnextyear").val()-$("#sppfrstyear").val() - 1);
-        nexmonths = $("#sppnextmonth").val();
-        monthcount = parseInt(frsmonths)+parseInt(months)+parseInt(nexmonths) + 1;
-    }else{
-        alert("Tahun kedua tidak boleh kurang tahun pertama");
-    }
-    spptotal = parseInt(originspp)*monthcount;
-    $("#spp").val(numberWithCommas(spptotal));
-    $("#spp_").val(numberWithCommas(spptotal));
-    if(test===true){
-        alert(monthcount);
-    };
+    });
+})
+.fail(function(err){
+    console.log("Error fetch json",err);
+});
+$("#cashpay").keyup(function(){
+    fillreturnmoney()
+});
+$("#cashpay").focus(function(){
+    $(this).select();
+})
+$("#cashpay").blur(function(){
+    $(this).val(numberWithCommas($(this).val()));
+})
+$(".affect-total").focus(function(){
+    $(this).val(removeCommas($(this).val().toString()));
+    $(this).select();
+})
+$(".affect-total").blur(function(){
+    $(this).val(numberWithCommas($(this).val().toString()));
+})
+togglespp();
+$("#sppcheckbox").change(function(){
+    togglespp();
+})
+togglebimbel();
+$("#bimbelcheckbox").change(function(){
+    togglebimbel();
+});
+$(".affect-total").keyup(function(){
+    filltotal();
+    fillreturnmoney();
+});
+$(".bimberperiod").adjustval({
+    firstmonth:$("#frstmonth"),
+    firstyear:$("#frstyear"),
+    nextmonth:$("#nextmonth"),
+    nextyear:$("#nextyear"),
+    hiddenval : $("#bimbel"),
+    shownval: $("#bimbel_"),
+    orival: $("#oribimbel")
+});
+$(".sppperiod").adjustval({
+    firstmonth:$("#sppfrstmonth"),
+    firstyear:$("#sppfrstyear"),
+    nextmonth:$("#sppnextmonth"),
+    nextyear:$("#sppnextyear"),
+    hiddenval : $("#spp"),
+    shownval: $("#spp_"),
+    orival: $("#orispp")
 });
