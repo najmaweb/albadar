0b371a896b276b587e271583e061f3376327275a
pujie <pw.prayitno@gmail.com>
2017-07-14 06:08:15 +0700

    allpaid for all payment on cashier

--- application/models/Mcashier.php
+++ application/models/Mcashier.php
@@ -16,6 +16,15 @@ class Mcashier extends CI_Model{
         }
         return $que->result()[0]->amnt;
     }
+    function getallpaid($nis,$year){
+        $sql = "select nis,sum(amount)spp from spp where nis='".$nis."' and cyear='".$year."'";
+        $ci = & get_instance();
+        $que = $ci->db->query($sql);
+        if($que->num_rows()===0){
+            return 0;
+        }
+        return $que->result()[0]->amnt;
+    }
     function gettotaltagihan($nis,$year){
         $sql = "select sum(b.amount)amnt from students a left outer join dupsbgroups b on b.id=a.dupsbgroup_id where nis='".$nis."' and a.year='".$year."' ";
         $ci = & get_instance();
