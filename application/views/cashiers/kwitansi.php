591745e59dfec2941620564ba5f4d0a6629a45bd
pujie <pw.prayitno@gmail.com>
2017-07-14 20:43:55 +0700

    adjust and format kwitansi

--- application/views/cashiers/kwitansi.php
+++ application/views/cashiers/kwitansi.php
@@ -42,14 +42,25 @@
         <tr><td colspan=2>&nbsp;</td><td colspan=2>&nbsp;</td><td colspan=2>&nbsp;</td></tr>
         <tr><td colspan=2>&nbsp;</td><td colspan=2>&nbsp;</td><td colspan=2>&nbsp;</td></tr>
         <tr><td class="line" colspan=6></td</tr>
-        <tr><td>Total Tagihan</td><td><?php echo "Rp. " .  number_format($totaltagihan);?></td><td colspan=2>&nbsp;</td><td>TOTAL</td><td class="rightaligned number"><?php echo number_format($total);?></td></tr>
-        <tr><td>Yang sudah dibayar</td><td><?php echo "Rp. " .  number_format($dupsbpaid);?></td><td colspan=2>&nbsp;</td><td></td><td></td></tr>
-        <tr><td>Sisa Tagihan</td><td><?php echo  "Rp. " . number_format($dupsbremain);?></td><td colspan=2>&nbsp;</td><td></td><td></td></tr>
+        <tr>
+            <td>Total Tagihan</td>
+            <td class="rightaligned number"><?php echo "Rp. " .  number_format($totaltagihan);?></td>
+            <td colspan=2>&nbsp;</td><td>TOTAL</td>
+            <td class="rightaligned number"><?php echo number_format($total);?></td>
+        </tr>
+        <tr><td>Yang sudah dibayar</td>
+            <td class="rightaligned number"><?php echo "Rp. " .  number_format($allpaid);?></td><td colspan=2>&nbsp;</td><td></td><td></td>
+        </tr>
+        <tr>
+            <td>Sisa Tagihan</td><td class="rightaligned number"><?php echo  "Rp. " . number_format($tagihanremain);?></td><td colspan=2>&nbsp;</td><td></td><td></td>
+        </tr>
         <tr><td>Status</td><td>Belum Lunas</td><td colspan=2>&nbsp;</td><td></td><td></td></tr>
         <tr><td>&nbsp;</td><td class="rightaligned number"></td><td colspan=2>&nbsp;</td><td colspan=2 class="centeraligned">Banjarsari, <?php echo date("d") . "-" . $periodmonths[removezero(date("m"))] . "-" . date("Y");?></td></tr>
         <tr><td>&nbsp;</td><td class="rightaligned number"></td><td colspan=2>&nbsp;</td><td>&nbsp;</td><td></td></tr>
         <tr><td>&nbsp;</td><td class="rightaligned number"></td><td colspan=2>&nbsp;</td><td class="centeraligned"></td><td></td></tr>
-        <tr><td>&nbsp;</td><td class="rightaligned number"></td><td colspan=2>&nbsp;</td><td colspan=2 class="centeraligned"><?php echo $_SESSION["username"];?></td></tr>
+        <tr><td>&nbsp;</td><td class="rightaligned number"></td><td colspan=2>&nbsp;</td>
+            <td colspan=2 class="centeraligned"><?php echo $_SESSION["username"];?></td>
+        </tr>
     </tbody>
 </table>
 <script type="text/javascript" src="/assets/js/kwitansi.js"></script>
