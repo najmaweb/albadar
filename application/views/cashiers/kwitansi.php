3b88b704c8d9e8595d6ef629d3b255878d12d0b0
pujie <pw.prayitno@gmail.com>
2017-07-17 21:43:01 +0700

    option for logo on kwitansi

--- application/views/cashiers/kwitansi.php
+++ application/views/cashiers/kwitansi.php
@@ -7,7 +7,13 @@
 <table id="rpt" width=100%>
     <thead>
         <tr><td width="16%"></td><td width="16%"></td><td width="16%"></td><td width="16%"></td><td width="16%"></td><td width="16%"></td></tr>
-        <tr><td class="image" colspan=2><img src="/assets/images/logo100x500.png"></td><td colspan=2 >KWITANSI</td><td id="nokwitansi" colspan=2 >No. EL/06/17/0001</td></tr>
+        <tr><td class="image" colspan=2>
+        <?php $showlogo = false;?>
+        <?php if($showlogo){?>
+        <img src="/assets/images/logo100x500.png">
+        <?php }?>
+        </td>
+        <td colspan=2 >KWITANSI</td><td id="nokwitansi" colspan=2 >No. EL/06/17/0001</td></tr>
     </thead>
     <tbody>
         <tr><td class="line" colspan=6></td</tr>
