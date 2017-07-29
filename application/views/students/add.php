60ba68bab7b609d27937e3f8bdc2d33106c1edf2
pujie <pw.prayitno@gmail.com>
2017-07-29 18:10:31 +0700

    add cancel url, remove unused script

--- application/views/students/add.php
+++ application/views/students/add.php
@@ -18,8 +18,6 @@
             <?php $this->load->view("commons/horizontalmenu");?>
                 <!-- content -->
                 <div class="col-md-10">
-
-
                     <div class="row">
                         <div class="col-lg-12">
                             <div class="panel panel-default bootstrap-admin-no-table-panel">
@@ -60,7 +58,7 @@
                                                 </div>
                                             </div>
                                             <button type="submit" class="btn btn-primary">Simpan Perubahan</button>
-                                            <button type="reset" class="btn btn-default">Batalkan</button>
+                                            <button type="reset" class="btn btn-default" id="btncancel">Batalkan</button>
                                         </fieldset>
                                     </form>
                                 </div>
@@ -70,36 +68,15 @@
                 </div>
             </div>
         </div>
-
         <!-- footer -->
         <?php $this->load->view("commons/footer");?>
         <?php $this->load->view("commons/assets");?>
-
         <script type="text/javascript" src="/assets/js/autocomplete/jquery-1.8.2.min.js"></script>
         <script type="text/javascript" src="/assets/js/autocomplete/jquery.autocomplete.js"></script>
         <script type="text/javascript">
-                var bts = [{"value":"170001-Ahmad Trianto (5c)","data":1},{"value":"170002-Irfan Hakim (5b)","data":2},{"value":"170003-Irwan Maulana (4d)","data":3}];
-
-                $('#focusedInput').autocomplete({
-                    lookup: bts,
-                    onSelect: function(suggestion) {
-                        $('#client_id').val(suggestion.data);
-                        console.log('suggestion',suggestion);
-                        //$('#selction-bts').html('You selected: ' + suggestion.value + ', ' + suggestion.data);
-                    },
-                    onHint: function (hint) {
-                        console.log('hint',hint);
-                        //$('#autocomplete-bts-x').val(hint);
-                    },
-                    onInvalidateSelection: function() {
-                        //$('#selction-bts').html('You selected: none');
-                    }
-                });
-        </script>
-        <script>
-        $("#cashpay").change(function(){
-            $("#returnmoney").val($(this).val());
-        });
+            $("#btncancel").click(function(){
+                window.location.href = "/students"
+            });
         </script>
     </body>
 </html>
